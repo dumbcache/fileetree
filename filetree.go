bf327a2b96731be77110da98dc51d058ef041db5
dumbcache <dumbcache@gmail.com>
2022-12-28 13:17:27 +0530

    added coloring

--- filetree.go
+++ filetree.go
@@ -2,6 +2,7 @@ package filetree
 
 import (
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"math/rand"
 	"os"
@@ -10,66 +11,70 @@ import (
 	"time"
 )
 
-var colors = map[string]string{
-	"Reset":   "\033[0m",
-	"Red":     "\033[31m",
-	"Green":   "\033[32m",
-	"Yellow":  "\033[33m",
-	"Blue":    "\033[34m",
-	"Purple":  "\033[35m",
-	"Cyan":    "\033[36m",
-	"Gray":    "\033[37m",
-	"BG":      "\033[40m",
-	"BgReset": "\033[49m",
-	"White":   "\033[97m",
+var colors = []string{
+	"\033[31m",
+	"\033[32m",
+	"\033[33m",
+	"\033[34m",
+	"\033[35m",
+	"\033[36m",
+	// "\033[37m",
 }
 var (
-	vLine = "\U00002502"
-	hLine = "\U00002500"
+	vLine      = "\U00002502"
+	hLine      = "\U00002500"
+	colorBG    = "\033[40m"
+	colorReset = "\033[0m"
+	bgReset    = "\033[49m"
+	space      = ""
 )
 
 func random() int {
 	s := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(s)
-	return r.Intn(10)
+	return r.Intn(6)
 }
 
 func execCommand(c string, args ...string) {
-	fmt.Print(colors["Blue"])
 	cmd := exec.Command(c, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Run()
-	fmt.Print(colors["Reset"])
 }
 
-func readFiles(dir string, padding string, args ...string) {
+func readFiles(path string, color string, padding string, args ...string) {
 
-	padding = vLine + padding
-	space := ""
+	padding = padding + color + vLine + colorReset
 	if len(args) != 0 {
+		padding = color + vLine + colorReset + space
 		space = args[0]
-		padding = vLine + space
 	}
-	files, _ := ioutil.ReadDir(dir)
-	// var tempFiles = make([]string, len(files))
-	// for _, file := range files {
-	// 	tempFiles = append(tempFiles, file.Name())
-	// }
-	// fmt.Println(tempFiles)
-	// sort.Strings(tempFiles)
+	files, _ := ioutil.ReadDir(path)
+	dirList := []fs.FileInfo{}
 	for _, file := range files {
-		fmt.Print(padding)
-		if file.IsDir() {
+		if !file.IsDir() {
+			fmt.Print(padding)
 			fmt.Println(hLine + " " + file.Name())
-			readFiles(dir+"/"+file.Name(), " "+padding)
-		} else {
-			fmt.Println(hLine + " " + file.Name())
+			continue
 		}
+		dirList = append(dirList, file)
+
+	}
+	dirColor := colors[random()]
+	for {
+		if dirColor != color {
+			break
+		}
+		dirColor = colors[random()]
+	}
+	for _, dir := range dirList {
+		fmt.Print(padding)
+		fmt.Println(hLine + dirColor + "./" + dir.Name() + colorReset)
+		readFiles(path+"/"+dir.Name(), dirColor, padding+space)
 	}
 }
 
-func FileTree(dir string) {
-	execCommand("clear")
-	fmt.Println(colors["Red"] + "files:" + colors["Reset"])
-	readFiles(dir, "")
+func FileTree(path string) {
+	initColor := colors[random()]
+	fmt.Println(initColor + path + colorReset)
+	readFiles(path, initColor, "", " ")
 }
